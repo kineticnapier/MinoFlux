edb5352f7c1d45f33c55e13f4c4b26fb976d3dd2
Kinetic Napier <u61819269@gmail.com>
2026-07-23 16:23:22 +0900

    Export CEM and replay APIs

--- src/minoflux_ai/__init__.py
+++ src/minoflux_ai/__init__.py
@@ -1,4 +1,18 @@
-from .benchmark import BenchmarkGame, BenchmarkResult, run_heuristic_benchmark, run_heuristic_game
+from .benchmark import (
+    BenchmarkGame,
+    BenchmarkResult,
+    record_heuristic_game,
+    run_heuristic_benchmark,
+    run_heuristic_game,
+)
+from .cem import (
+    TRAINABLE_WEIGHT_NAMES,
+    CEMConfig,
+    CEMGeneration,
+    CEMResult,
+    benchmark_fitness,
+    train_cem,
+)
 from .features import BoardFeatures, column_heights, extract_board_features
 from .heuristic import (
     DEFAULT_WEIGHTS,
@@ -13,10 +27,23 @@ from .heuristic import (
     save_weights,
     score_features,
 )
+from .replay import (
+    REPLAY_FORMAT,
+    Replay,
+    ReplayStep,
+    ReplaySummary,
+    apply_replay_step,
+    load_replay,
+    replay_to_game,
+    save_replay,
+)
 
 __all__ = [
-    "BenchmarkGame", "BenchmarkResult", "BoardFeatures", "DEFAULT_WEIGHTS", "HeuristicWeights",
-    "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures", "choose_placement", "column_heights",
-    "evaluate_placement", "extract_board_features", "load_weights", "rank_placements",
-    "run_heuristic_benchmark", "run_heuristic_game", "save_weights", "score_features",
+    "BenchmarkGame", "BenchmarkResult", "BoardFeatures", "CEMConfig", "CEMGeneration", "CEMResult",
+    "DEFAULT_WEIGHTS", "HeuristicWeights", "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures",
+    "REPLAY_FORMAT", "Replay", "ReplayStep", "ReplaySummary", "TRAINABLE_WEIGHT_NAMES",
+    "apply_replay_step", "benchmark_fitness", "choose_placement", "column_heights", "evaluate_placement",
+    "extract_board_features", "load_replay", "load_weights", "rank_placements", "record_heuristic_game",
+    "replay_to_game", "run_heuristic_benchmark", "run_heuristic_game", "save_replay", "save_weights",
+    "score_features", "train_cem",
 ]
