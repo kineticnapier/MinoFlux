d376f6e4069fc6820cb44d132726c3e3a81739d4
Kinetic Napier <u61819269@gmail.com>
2026-07-23 20:40:12 +0900

    Export offensive fitness and model promotion APIs

--- src/minoflux_ai/__init__.py
+++ src/minoflux_ai/__init__.py
@@ -6,11 +6,18 @@ from .benchmark import (
     run_heuristic_game,
 )
 from .cem import (
+    ATTACK_SPIN_FITNESS,
+    BALANCED_FITNESS,
+    FITNESS_PROFILE_ATTACK_SPIN,
+    FITNESS_PROFILE_BALANCED,
+    FITNESS_PROFILE_NAMES,
     TRAINABLE_WEIGHT_NAMES,
     CEMConfig,
     CEMGeneration,
     CEMResult,
+    FitnessProfile,
     benchmark_fitness,
+    resolve_fitness_profile,
     train_cem,
 )
 from .features import BoardFeatures, column_heights, extract_board_features
@@ -26,6 +33,13 @@ from .heuristic import (
     rank_placements,
     save_weights,
     score_features,
+)
+from .promotion import (
+    PromotionConfig,
+    PromotionResult,
+    bootstrap_champion,
+    compare_candidate_to_champion,
+    evaluate_and_promote_model,
 )
 from .replay import (
     LEGACY_REPLAY_FORMAT,
@@ -50,13 +64,16 @@ from .search import (
 )
 
 __all__ = [
-    "BenchmarkGame", "BenchmarkResult", "BoardFeatures", "CEMConfig", "CEMGeneration", "CEMResult",
-    "DEFAULT_SEARCH_CONFIG", "DEFAULT_WEIGHTS", "DIRECT_SEARCH_CONFIG", "HeuristicWeights",
-    "LEGACY_REPLAY_FORMAT", "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures", "REPLAY_FORMAT",
+    "ATTACK_SPIN_FITNESS", "BALANCED_FITNESS", "BenchmarkGame", "BenchmarkResult", "BoardFeatures",
+    "CEMConfig", "CEMGeneration", "CEMResult", "DEFAULT_SEARCH_CONFIG", "DEFAULT_WEIGHTS",
+    "DIRECT_SEARCH_CONFIG", "FITNESS_PROFILE_ATTACK_SPIN", "FITNESS_PROFILE_BALANCED",
+    "FITNESS_PROFILE_NAMES", "FitnessProfile", "HeuristicWeights", "LEGACY_REPLAY_FORMAT", "MODEL_FORMAT",
+    "PlacementEvaluation", "PlacementFeatures", "PromotionConfig", "PromotionResult", "REPLAY_FORMAT",
     "Replay", "ReplayStep", "ReplaySummary", "SearchAction", "SearchChoice", "SearchConfig",
     "TRAINABLE_WEIGHT_NAMES", "apply_replay_step", "apply_search_action", "benchmark_fitness",
-    "choose_placement", "choose_search_action", "column_heights", "evaluate_placement",
+    "bootstrap_champion", "choose_placement", "choose_search_action", "column_heights",
+    "compare_candidate_to_champion", "evaluate_and_promote_model", "evaluate_placement",
     "extract_board_features", "load_replay", "load_weights", "rank_placements", "rank_search_actions",
-    "record_heuristic_game", "replay_to_game", "run_heuristic_benchmark", "run_heuristic_game",
-    "save_replay", "save_weights", "score_features", "train_cem",
+    "record_heuristic_game", "replay_to_game", "resolve_fitness_profile", "run_heuristic_benchmark",
+    "run_heuristic_game", "save_replay", "save_weights", "score_features", "train_cem",
 ]
