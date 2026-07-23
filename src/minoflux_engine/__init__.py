8f974cfb5ad50c151a84ac1f01ed0ac073ab8c9f
Kinetic Napier <u61819269@gmail.com>
2026-07-23 23:29:45 +0900

    Export B2B Charging and versus engine APIs

--- src/minoflux_engine/__init__.py
+++ src/minoflux_engine/__init__.py
@@ -1,4 +1,5 @@
-from .game import DEFAULT_LOCK_DELAY_MS, DEFAULT_LOCK_RESET_LIMIT, Game
+from .b2b import B2BOutcome, SURGE_START_CHAIN, resolve_b2b_charging, split_surge
+from .game import DEFAULT_LOCK_DELAY_MS, DEFAULT_LOCK_RESET_LIMIT, GARBAGE_CELL, Game
 from .pieces import BOARD_HEIGHT, BOARD_WIDTH, HIDDEN_ROWS, PIECE_NAMES, VISIBLE_HEIGHT
 from .spin import (
     T_SPIN,
@@ -10,10 +11,13 @@ from .spin import (
     T_SPIN_TRIPLE,
 )
 from .state import GameSnapshot, LockResult, Placement
+from .versus import GarbagePacket, GarbageQueue, VersusMatch, VersusResolution, VersusSide
 
 __all__ = [
-    "BOARD_HEIGHT", "BOARD_WIDTH", "DEFAULT_LOCK_DELAY_MS", "DEFAULT_LOCK_RESET_LIMIT",
-    "Game", "GameSnapshot", "HIDDEN_ROWS", "LockResult", "PIECE_NAMES", "Placement",
-    "T_SPIN", "T_SPIN_DOUBLE", "T_SPIN_EVENTS", "T_SPIN_MINI", "T_SPIN_MINI_SINGLE",
-    "T_SPIN_SINGLE", "T_SPIN_TRIPLE", "VISIBLE_HEIGHT",
+    "B2BOutcome", "BOARD_HEIGHT", "BOARD_WIDTH", "DEFAULT_LOCK_DELAY_MS",
+    "DEFAULT_LOCK_RESET_LIMIT", "GARBAGE_CELL", "Game", "GameSnapshot", "GarbagePacket",
+    "GarbageQueue", "HIDDEN_ROWS", "LockResult", "PIECE_NAMES", "Placement",
+    "SURGE_START_CHAIN", "T_SPIN", "T_SPIN_DOUBLE", "T_SPIN_EVENTS", "T_SPIN_MINI",
+    "T_SPIN_MINI_SINGLE", "T_SPIN_SINGLE", "T_SPIN_TRIPLE", "VISIBLE_HEIGHT",
+    "VersusMatch", "VersusResolution", "VersusSide", "resolve_b2b_charging", "split_surge",
 ]
