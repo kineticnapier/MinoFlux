8de5eefda8a228baaa86b46d0d4db488c7a3aede
Kinetic Napier <u61819269@gmail.com>
2026-07-23 18:35:45 +0900

    Export beam-search and replay v2 APIs

--- src/minoflux_ai/__init__.py
+++ src/minoflux_ai/__init__.py
@@ -28,6 +28,7 @@ from .heuristic import (
     score_features,
 )
 from .replay import (
+    LEGACY_REPLAY_FORMAT,
     REPLAY_FORMAT,
     Replay,
     ReplayStep,
@@ -37,13 +38,25 @@ from .replay import (
     replay_to_game,
     save_replay,
 )
+from .search import (
+    DEFAULT_SEARCH_CONFIG,
+    DIRECT_SEARCH_CONFIG,
+    SearchAction,
+    SearchChoice,
+    SearchConfig,
+    apply_search_action,
+    choose_search_action,
+    rank_search_actions,
+)
 
 __all__ = [
     "BenchmarkGame", "BenchmarkResult", "BoardFeatures", "CEMConfig", "CEMGeneration", "CEMResult",
-    "DEFAULT_WEIGHTS", "HeuristicWeights", "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures",
-    "REPLAY_FORMAT", "Replay", "ReplayStep", "ReplaySummary", "TRAINABLE_WEIGHT_NAMES",
-    "apply_replay_step", "benchmark_fitness", "choose_placement", "column_heights", "evaluate_placement",
-    "extract_board_features", "load_replay", "load_weights", "rank_placements", "record_heuristic_game",
-    "replay_to_game", "run_heuristic_benchmark", "run_heuristic_game", "save_replay", "save_weights",
-    "score_features", "train_cem",
+    "DEFAULT_SEARCH_CONFIG", "DEFAULT_WEIGHTS", "DIRECT_SEARCH_CONFIG", "HeuristicWeights",
+    "LEGACY_REPLAY_FORMAT", "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures", "REPLAY_FORMAT",
+    "Replay", "ReplayStep", "ReplaySummary", "SearchAction", "SearchChoice", "SearchConfig",
+    "TRAINABLE_WEIGHT_NAMES", "apply_replay_step", "apply_search_action", "benchmark_fitness",
+    "choose_placement", "choose_search_action", "column_heights", "evaluate_placement",
+    "extract_board_features", "load_replay", "load_weights", "rank_placements", "rank_search_actions",
+    "record_heuristic_game", "replay_to_game", "run_heuristic_benchmark", "run_heuristic_game",
+    "save_replay", "save_weights", "score_features", "train_cem",
 ]
