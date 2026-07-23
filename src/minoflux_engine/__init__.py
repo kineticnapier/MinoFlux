97ee0adffb64db6a82f44aca63881ec7ba6c3199
Kinetic Napier <u61819269@gmail.com>
2026-07-23 15:16:44 +0900

    Export lock delay defaults

--- src/minoflux_engine/__init__.py
+++ src/minoflux_engine/__init__.py
@@ -1,8 +1,9 @@
-from .game import Game
+from .game import DEFAULT_LOCK_DELAY_MS, DEFAULT_LOCK_RESET_LIMIT, Game
 from .pieces import BOARD_HEIGHT, BOARD_WIDTH, HIDDEN_ROWS, PIECE_NAMES, VISIBLE_HEIGHT
 from .state import GameSnapshot, LockResult, Placement
 
 __all__ = [
-    "BOARD_HEIGHT", "BOARD_WIDTH", "Game", "GameSnapshot", "HIDDEN_ROWS",
-    "LockResult", "PIECE_NAMES", "Placement", "VISIBLE_HEIGHT",
+    "BOARD_HEIGHT", "BOARD_WIDTH", "DEFAULT_LOCK_DELAY_MS", "DEFAULT_LOCK_RESET_LIMIT",
+    "Game", "GameSnapshot", "HIDDEN_ROWS", "LockResult", "PIECE_NAMES", "Placement",
+    "VISIBLE_HEIGHT",
 ]
