38af384f767100c2f239e6414901232cb68ebe5a
Kinetic Napier <u61819269@gmail.com>
2026-07-23 22:44:10 +0900

    Export exact T-spin event names

--- src/minoflux_engine/__init__.py
+++ src/minoflux_engine/__init__.py
@@ -1,9 +1,19 @@
 from .game import DEFAULT_LOCK_DELAY_MS, DEFAULT_LOCK_RESET_LIMIT, Game
 from .pieces import BOARD_HEIGHT, BOARD_WIDTH, HIDDEN_ROWS, PIECE_NAMES, VISIBLE_HEIGHT
+from .spin import (
+    T_SPIN,
+    T_SPIN_DOUBLE,
+    T_SPIN_EVENTS,
+    T_SPIN_MINI,
+    T_SPIN_MINI_SINGLE,
+    T_SPIN_SINGLE,
+    T_SPIN_TRIPLE,
+)
 from .state import GameSnapshot, LockResult, Placement
 
 __all__ = [
     "BOARD_HEIGHT", "BOARD_WIDTH", "DEFAULT_LOCK_DELAY_MS", "DEFAULT_LOCK_RESET_LIMIT",
     "Game", "GameSnapshot", "HIDDEN_ROWS", "LockResult", "PIECE_NAMES", "Placement",
-    "VISIBLE_HEIGHT",
+    "T_SPIN", "T_SPIN_DOUBLE", "T_SPIN_EVENTS", "T_SPIN_MINI", "T_SPIN_MINI_SINGLE",
+    "T_SPIN_SINGLE", "T_SPIN_TRIPLE", "VISIBLE_HEIGHT",
 ]
