34610b2013dfdf04292d694eb1a41900923a04b3
Kinetic Napier <u61819269@gmail.com>
2026-07-24 20:00:42 +0900

    Export capture importer and versus AI APIs

--- src/minoflux_ai/__init__.py
+++ src/minoflux_ai/__init__.py
@@ -34,7 +34,6 @@ from .heuristic import (
     save_weights,
     score_features,
 )
-from .reachability import reachable_placements
 from .promotion import (
     PromotionConfig,
     PromotionResult,
@@ -42,6 +41,7 @@ from .promotion import (
     compare_candidate_to_champion,
     evaluate_and_promote_model,
 )
+from .reachability import reachable_placements
 from .replay import (
     LEGACY_REPLAY_FORMAT,
     LEGACY_REPLAY_FORMAT_V2,
@@ -64,19 +64,52 @@ from .search import (
     choose_search_action,
     rank_search_actions,
 )
+from .tetrio_capture import (
+    CAPTURE_DATASET_FORMAT,
+    CapturePlacement,
+    CaptureSample,
+    build_capture_samples,
+    capture_summary,
+    load_tetrio_capture,
+    normalize_board,
+    save_capture_dataset,
+)
+from .versus_benchmark import (
+    VersusBenchmarkResult,
+    VersusGameResult,
+    run_versus_benchmark,
+    run_versus_game,
+)
+from .versus_search import (
+    DEFAULT_VERSUS_SEARCH_CONFIG,
+    DEFAULT_VERSUS_WEIGHTS,
+    VersusChoice,
+    VersusSearchConfig,
+    VersusWeights,
+    choose_versus_action,
+    clone_versus_match,
+    score_versus_state,
+)
 
 __all__ = [
     "ATTACK_SPIN_FITNESS", "BALANCED_FITNESS", "BenchmarkGame", "BenchmarkResult",
-    "BoardFeatures", "CEMConfig", "CEMGeneration", "CEMResult", "DEFAULT_SEARCH_CONFIG",
-    "DEFAULT_WEIGHTS", "DIRECT_SEARCH_CONFIG", "FITNESS_PROFILE_ATTACK_SPIN",
-    "FITNESS_PROFILE_BALANCED", "FITNESS_PROFILE_NAMES", "FitnessProfile", "HeuristicWeights",
-    "LEGACY_REPLAY_FORMAT", "LEGACY_REPLAY_FORMAT_V2", "MODEL_FORMAT", "PlacementEvaluation",
-    "PlacementFeatures", "PromotionConfig", "PromotionResult", "REPLAY_FORMAT", "Replay", "ReplayStep", "ReplaySummary", "SearchAction",
-    "SearchChoice", "SearchConfig", "TRAINABLE_WEIGHT_NAMES", "apply_replay_step",
-    "apply_search_action", "benchmark_fitness", "bootstrap_champion", "choose_placement", "choose_search_action", "compare_candidate_to_champion",
-    "column_heights", "evaluate_and_promote_model", "evaluate_placement", "extract_board_features", "load_replay",
-    "load_weights", "rank_placements", "rank_search_actions", "reachable_placements",
+    "BoardFeatures", "CAPTURE_DATASET_FORMAT", "CEMConfig", "CEMGeneration", "CEMResult",
+    "CapturePlacement", "CaptureSample", "DEFAULT_SEARCH_CONFIG", "DEFAULT_VERSUS_SEARCH_CONFIG",
+    "DEFAULT_VERSUS_WEIGHTS", "DEFAULT_WEIGHTS", "DIRECT_SEARCH_CONFIG",
+    "FITNESS_PROFILE_ATTACK_SPIN", "FITNESS_PROFILE_BALANCED", "FITNESS_PROFILE_NAMES",
+    "FitnessProfile", "HeuristicWeights", "LEGACY_REPLAY_FORMAT", "LEGACY_REPLAY_FORMAT_V2",
+    "MODEL_FORMAT", "PlacementEvaluation", "PlacementFeatures", "PromotionConfig",
+    "PromotionResult", "REPLAY_FORMAT", "Replay", "ReplayStep", "ReplaySummary", "SearchAction",
+    "SearchChoice", "SearchConfig", "TRAINABLE_WEIGHT_NAMES", "VersusBenchmarkResult",
+    "VersusChoice", "VersusGameResult", "VersusSearchConfig", "VersusWeights",
+    "apply_replay_step", "apply_search_action", "benchmark_fitness", "bootstrap_champion",
+    "build_capture_samples", "capture_summary", "choose_placement", "choose_search_action",
+    "choose_versus_action", "clone_versus_match", "column_heights",
+    "compare_candidate_to_champion", "evaluate_and_promote_model", "evaluate_placement",
+    "extract_board_features", "load_replay", "load_tetrio_capture", "load_weights",
+    "normalize_board", "rank_placements", "rank_search_actions", "reachable_placements",
     "record_heuristic_game", "replay_to_game", "resolve_fitness_profile",
-    "run_heuristic_benchmark", "run_heuristic_game", "save_replay", "save_weights",
-    "score_features", "train_cem",
+    "run_heuristic_benchmark", "run_heuristic_game", "run_versus_benchmark", "run_versus_game",
+    "save_capture_dataset", "save_replay", "save_weights", "score_features",
+    "score_versus_state", "train_cem",
 ]
