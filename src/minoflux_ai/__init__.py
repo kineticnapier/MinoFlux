98861c2a01fe197aa649ea101dfc9d88e85a1069
Kinetic Napier <u61819269@gmail.com>
2026-09-13 02:37:58 +0900

    Install fused versus batch fast path

--- src/minoflux_ai/__init__.py
+++ src/minoflux_ai/__init__.py
@@ -88,6 +88,12 @@ from .neural_search_fast import (
 
 _install_neural_search_fast_path()
 
+from .versus_batch_fast import (
+    install_versus_batch_fast_path as _install_versus_batch_fast_path,
+)
+
+_install_versus_batch_fast_path()
+
 from .promotion import (
     PromotionConfig,
     PromotionResult,
