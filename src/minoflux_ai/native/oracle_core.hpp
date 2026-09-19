34666b2d90ab9288e0df7927ac4c47fe28849e5c
Kinetic Napier <u61819269@gmail.com>
2026-09-20 00:43:52 +0900

    Profile native oracle search stages

--- src/minoflux_ai/native/oracle_core.hpp
+++ src/minoflux_ai/native/oracle_core.hpp
@@ -78,8 +78,6 @@ struct TransitionResult {
 struct ReachabilityProfile {
     uint64_t calls = 0;
     uint64_t generated_moves = 0;
-    uint64_t cache_hits = 0;
-    uint64_t cache_misses = 0;
     double total_seconds = 0.0;
     double setup_seconds = 0.0;
     double bfs_seconds = 0.0;
@@ -89,6 +87,16 @@ struct ReachabilityProfile {
     double placement_seconds = 0.0;
 };
 
+struct SearchProfile {
+    uint64_t feature_calls = 0;
+    uint64_t expanded_children = 0;
+    uint64_t dedup_hits = 0;
+    uint64_t dedup_replacements = 0;
+    double feature_seconds = 0.0;
+    double dedup_seconds = 0.0;
+    double prune_seconds = 0.0;
+};
+
 Piece piece_from_char(char value);
 char piece_to_char(Piece piece);
 void register_reachability_table(
@@ -96,10 +104,10 @@ void register_reachability_table(
     bool allow_180,
     std::shared_ptr<const minoflux::reachability::Table> table
 );
-void begin_reachability_cache();
-void end_reachability_cache();
 void begin_reachability_profile();
 ReachabilityProfile end_reachability_profile();
+void begin_search_profile();
+SearchProfile end_search_profile();
 std::vector<Move> reachable_moves(
     const std::array<uint16_t, kHeight>& rows,
     Piece piece,
