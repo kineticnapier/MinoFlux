336315dd3dd0ea79c534776074e309de10f7cdc7
Kinetic Napier <u61819269@gmail.com>
2026-09-20 23:57:30 +0900

    revert: drop AVX2 oracle experiment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 
 oracle_compile_args = (
-    ["/O2", "/Ob3", "/Oi", "/Ot", "/arch:AVX2"]
+    ["/O2", "/Ob3", "/Oi", "/Ot"]
     if sys.platform == "win32"
     else ["-O3"]
 )
