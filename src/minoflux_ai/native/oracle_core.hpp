28cb870702953573c4fa5fecac921265c7fac5d2
Kinetic Napier <u61819269@gmail.com>
2026-09-21 10:21:48 +0900

    perf: reuse oracle move generation buffers

--- src/minoflux_ai/native/oracle_core.hpp
+++ src/minoflux_ai/native/oracle_core.hpp
@@ -3,6 +3,7 @@
 #include "reachability_core.hpp"
 
 #include <array>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <span>
@@ -36,6 +37,28 @@ struct Move {
     int8_t kick_index = -1;
     int8_t rotation_from = -1;
     int8_t rotation_to = -1;
+};
+
+class MoveRange {
+public:
+    MoveRange() = default;
+    MoveRange(const Move* data, std::size_t size) noexcept : data_(data), size_(size) {}
+
+    const Move* begin() const noexcept { return data_; }
+    const Move* end() const noexcept { return size_ == 0 ? data_ : data_ + size_; }
+    std::size_t size() const noexcept { return size_; }
+    bool empty() const noexcept { return size_ == 0; }
+
+    operator std::vector<Move>() const {
+        if (size_ == 0) {
+            return {};
+        }
+        return std::vector<Move>(data_, data_ + size_);
+    }
+
+private:
+    const Move* data_ = nullptr;
+    std::size_t size_ = 0;
 };
 
 struct State {
@@ -137,7 +160,7 @@ void begin_reachability_profile();
 ReachabilityProfile end_reachability_profile();
 void begin_search_profile();
 SearchProfile end_search_profile();
-std::vector<Move> reachable_moves(
+MoveRange reachable_moves(
     const std::array<uint16_t, kHeight>& rows,
     Piece piece,
     bool allow_180,
