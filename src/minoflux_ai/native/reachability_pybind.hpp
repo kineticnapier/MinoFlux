059e63d6b537e8c63644d4f262685fa9cb91068e
Kinetic Napier <u61819269@gmail.com>
2026-09-21 20:03:54 +0900

    revert: drop combined reachability hot path experiment

--- src/minoflux_ai/native/reachability_pybind.hpp
+++ src/minoflux_ai/native/reachability_pybind.hpp
@@ -113,7 +113,6 @@ inline std::shared_ptr<Table> table_from_python(
         }
         table->state_group_offsets.push_back(static_cast<uint32_t>(table->group_kick_offsets.size() - 1));
     }
-    finalize_rotation_table(*table);
     return table;
 }
 
